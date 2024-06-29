f1eeb982539e52f3c92733d684972d3250673cbb
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-06-29 18:11:17 +0530

    feature: landing page basic

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,14 +1,13 @@
 <script> 
 </script> 
 
-<nav class="flex items-center justify-between p-6">
+<nav class="flex items-center  p-6 mx-auto max-w-[70%] mb-5">
   <div class="flex justify-start">
-    <img class="w-24 h-auto mr-4" src="./Designer.png" alt="logo">
+    <img class="w-24 h-auto mr-4 rounded-lg" src="./Designer.png" alt="logo">
   </div>
   <div class="flex-grow flex justify-center gap-4">
-    <a href="/text" class="text-slate-950 font-semibold  hover:text-gray-500">Text</a>
-    <a href="/image" class="text-slate-950 hover:text-gray-500">Image</a>
-    <a href="/finance" class="text-slate-950 hover:text-gray-500">Finance</a>
+    <a href="/text" class="text-slate-950 font-bold hover:text-gray-500">Text</a>
+    <a href="/image" class="text-slate-950 font-bold hover:text-gray-500">Image</a>
+    <a href="/finance" class="text-slate-950 font-bold hover:text-gray-500">Finance</a>
   </div>
 </nav>
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script> 
 </script> 
 
-<div > 
-    <h1> ToolSmith : A versatile Toolkit </h1>
-    <h2> Find the right tool for your needs </h2>
-    
+<div class="flex flex-col items-center justify-center justify-items-center mt-20 ">
+  <h1 class="font-extrabold text-5xl mb-4 text-center">ToolSmith : Versatile Tools </h1>
+  <h1 class="font-extrabold text-5xl text-center mb-8"> for the Modern Web </h1>
+
+
+  <h2 class="font-bold text-xl text-slate-600">Tools for Finance, Text, Image, PDF and Much more</h2>
+
+
+</div>
+
+  <div class="flex justify-evenly mt-20"> 
+    <img class="w-72  h-auto rounded-lg" src="./pic1.png" alt="pic1"/> 
+<p class="content-center text-slate-800 font-bold"> An all in one tools place for your needs </p>
+
     </div>
