63e4650cb8233a4ad0599b5062b04d7513eb3f4a
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-06-29 16:06:10 +0530

    feature: navbar started

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script> 
+</script> 
+
+<nav class="flex items-center justify-between p-6">
+  <div class="flex justify-start">
+    <img class="w-24 h-auto mr-4" src="./Designer.png" alt="logo">
+  </div>
+  <div class="flex-grow flex justify-center gap-4">
+    <a href="/text" class="text-slate-950 font-semibold  hover:text-gray-500">Text</a>
+    <a href="/image" class="text-slate-950 hover:text-gray-500">Image</a>
+    <a href="/finance" class="text-slate-950 hover:text-gray-500">Finance</a>
+  </div>
+</nav>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script> 
+</script> 
+
+<div > 
+    <h1> ToolSmith : A versatile Toolkit </h1>
+    <h2> Find the right tool for your needs </h2>
+    
+    </div>
