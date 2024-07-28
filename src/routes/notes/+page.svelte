3d4704ae12272f9ce07878b6d925dcfb5941a92e
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-07-28 12:07:45 +0530

    notes completed

---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+import NoteSidebar from '$lib/components/Notes/NoteSidebar.svelte';
+import NoteEditor from '$lib/components/Notes/NoteEditor.svelte';
+import { fetchNotes } from '$lib/stores/NoteStore.js';
+import { onMount } from 'svelte';
+
+let selectedNoteId = null;
+
+function handleNoteSelect(event) {
+    selectedNoteId = event.detail.noteId;
+}
+
+onMount(fetchNotes);
+</script>
+
+<div class="flex">
+    <div class="w-1/3">
+        <NoteSidebar on:selectnote={handleNoteSelect} />
+    </div>
+    <div class="w-2/3">
+        {#if selectedNoteId}
+            <NoteEditor noteId={selectedNoteId} />
+        {:else}
+            <p>Select a note or create a new one</p>
+        {/if}
+    </div>
+</div>
+
+
